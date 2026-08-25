e14b2ba414457d8cbd87b6568accfa1b1bbc8f0a
KristinaTomash <kristitomsh@gmail.com>
2026-08-25 13:59:59 +0300

    Add 4

--- src/test/java/com/demoqa/tests/WidgetsTests.java
+++ src/test/java/com/demoqa/tests/WidgetsTests.java
@@ -3,7 +3,9 @@ package com.demoqa.tests;
 import com.demoqa.core.TestBase;
 import com.demoqa.pages.HomePage;
 import com.demoqa.pages.SidePanel;
+import com.demoqa.pages.widgets.MenuPage;
 import com.demoqa.pages.widgets.SelectPage;
+import com.demoqa.pages.widgets.SliderPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.FindBy;
@@ -25,4 +27,33 @@ public class WidgetsTests extends TestBase {
                 .verifyColor();
 
     }
+    @Test
+    public void multiSelectDropDownTest(){
+        sidePanel.getSelectMenu();
+        select.multiSelect(new String[]{"Green","Red"})
+                .verifyMultiSelect(new String[]{"Green","Red"});
+    }
+    @Test
+    public void standardMultiSelectTest(){
+        sidePanel.getSelectMenu();
+        select.verifySelectedCar("opel","rgba(223, 197, 224, 1)");
+    }
+
+    @Test
+    public void hoverMouseOnMenuTest(){
+        sidePanel.getMenu();
+        new MenuPage(driver).hoverMouseOnMenu()
+                .verifySubMenu();
+
+    }
+    @Test
+    public void sliderTest(){
+        sidePanel.getSlider();
+        new SliderPage(driver).moveSlider()
+                .verifySliderValue("100")
+        ;
+    }
+
+
+
 }
